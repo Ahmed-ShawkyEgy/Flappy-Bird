7bb1a12d253ecf6e4222f50300e9d3bcc09acaa2
Ahmed-ShawkyEgy <ahmed.shawkyegy@gmail.com>
2019-04-05 16:27:09 +0200

    render bird image

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 push = require 'push'
+Class = require 'class'
+require "bird"
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -16,6 +18,8 @@ local BACKGROUND_SCROLL_SPEED = 30
 local GROUND_SCROLL_SPEED = 60
 
 local BACKGROUND_LOOPING_POINT = 413
+
+bird = Bird()
 
 function love.load ()
   love.graphics.setDefaultFilter('nearest','nearest')
@@ -52,6 +56,7 @@ function love.draw()
   love.graphics.draw(background,-backgroundScroll,0)
   love.graphics.draw(ground,-groundScroll,VIRTUAL_HEIGHT-16)
 
+  bird:render()
 
   push:finish()
 end
