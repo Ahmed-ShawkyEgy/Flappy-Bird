13c765d5946d646429f5c67d58bf031cfdb4ed30
Ahmed-ShawkyEgy <ahmed.shawkyegy@gmail.com>
2019-04-05 19:17:32 +0200

    add & implement score state

--- main.lua
+++ main.lua
@@ -8,6 +8,7 @@ require "PipePair"
 require 'StateMachine'
 require 'states/BaseState'
 require 'states/PlayState'
+require 'states/ScoreState'
 require 'states/TitleScreenState'
 
 WINDOW_WIDTH = 1280
@@ -61,6 +62,7 @@ function love.load ()
   gStateMachine = StateMachine {
          ['title'] = function() return TitleScreenState() end,
          ['play'] = function() return PlayState() end,
+         ['score'] = function() return ScoreState() end
      }
   gStateMachine:change('title')
 
